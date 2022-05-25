5d138da172bc036c3c8463a3d5108fd04ae5352b
AyazCiplak <97311304+AyazCiplak@users.noreply.github.com>
2022-05-25 13:42:51 -0400

    Restructured menu functions

--- src/managementsystem/Menu.java
+++ src/managementsystem/Menu.java
@@ -7,76 +7,67 @@ public class Menu {
     PriorityQueue queue = new PriorityQueue();
 
     void initialMessage() {
-        System.out.println("\nThis program is designed to simulate an emergency room priority queue, " +
-                "with patients being sorted based on an assigned numerical priority value. \nThe list can be manipulated " +
-                "in a variety of ways - patients can be added, removed, reassigned a new priority, etc.\n" +
-                "Note: A lower priority means a more urgent patient.\n");
+        System.out.println("""
+                This program is designed to simulate an emergency room priority queue, with patients being sorted based on an assigned numerical priority value.\s
+                The list can be manipulated in a variety of ways - patients can be added, removed, reassigned a new priority, etc.
+                Note: A lower priority means a more urgent patient.
+                """);
     }
 
     void mainOptions() {
 
         Scanner mainOptionInt = new Scanner(System.in);
-        System.out.println("MAIN MENU:\n" +
-                "------------------\n" +
-                "1 - Add Menu\n" +
-                "2 - Remove Menu\n" +
-                "3 - Find Menu\n" +
-                "4 - Change a patient's priority\n" +
-                "5 - List patients (ordered by priority) \n" +
-                "6 - Exit program\n\n" +
-                "Selection (enter a number): ");
+        System.out.println("""
+                MAIN MENU:
+                ------------------
+                1 - Add Menu
+                2 - Remove Menu
+                3 - Find Menu
+                4 - Change a patient's priority
+                5 - List patients (ordered by priority)\s
+                6 - Exit program
+
+                Selection (enter a number):\s""");
 
         int mainSelectionInt = mainOptionInt.nextInt();
 
-        switch(mainSelectionInt) {
-            case 1:
-                this.addMenu();
-                break;
-            case 2:
-                this.removeMenu();
-                break;
-            case 3:
-                this.findMenu();
-                break;
-            case 4:
-                this.priorityChange();
-                break;
-            case 5:
-                this.listPatients();
-                break;
-            case 6:
-                System.out.println("Program exited.");
-                break;
-            default:
+        switch (mainSelectionInt) {
+            case 1 -> this.addMenu();
+            case 2 -> this.removeMenu();
+            case 3 -> this.findMenu();
+            case 4 -> this.priorityChange();
+            case 5 -> this.listPatients();
+            case 6 -> System.out.println("Program exited.");
+            default -> {
                 System.out.println("Invalid selection\n");
                 this.mainOptions();
-                break;
+            }
         }
 
     }
 
-    
+
     void addMenu() {
 
         Scanner addOptionInt = new Scanner(System.in);
-        System.out.println("ADD MENU:\n" +
-                "------------------\n" +
-                "1 - Add patient and assign priority\n" +
-                "2 - Add patient to end of queue\n" +
-                "3 - Go back\n\n" +
-                "Selection (enter a number): ");
+        System.out.println("""
+                ADD MENU:
+                ------------------
+                1 - Add patient and assign priority
+                2 - Add patient to end of queue
+                3 - Go back
+
+                Selection (enter a number):\s""");
 
         int addSelectionInt = addOptionInt.nextInt();
 
-        switch(addSelectionInt) {
-            case 1:
+        switch (addSelectionInt) {
+            case 1 -> {
                 Scanner addName = new Scanner(System.in);
                 Scanner addPriority = new Scanner(System.in);
-
                 System.out.println("Name: ");
                 String name = addName.next();
-
-                if(queue.contains(name)){
+                if (queue.contains(name)) {
 
                     this.addFailure();
 
@@ -88,14 +79,12 @@ public class Menu {
                     queue.add(name, priority);
                     this.addSuccess();
                 }
-                break;
-            case 2:
+            }
+            case 2 -> {
                 Scanner addJustName = new Scanner(System.in);
-
                 System.out.println("Name: ");
                 String justName = addJustName.next();
-
-                if(queue.contains(justName)){
+                if (queue.contains(justName)) {
 
                     this.addFailure();
 
@@ -104,15 +93,12 @@ public class Menu {
                     queue.add(justName);
                     this.addSuccess();
                 }
-
-                break;
-            case 3:
-                this.mainOptions();
-                break;
-            default:
+            }
+            case 3 -> this.mainOptions();
+            default -> {
                 System.out.println("Invalid selection\n");
                 this.mainOptions();
-                break;
+            }
         }
     }
 
@@ -129,57 +115,57 @@ public class Menu {
     void removeMenu() {
 
         Scanner removeOptionInt = new Scanner(System.in);
-        System.out.println("REMOVE MENU:\n" +
-                "------------------\n" +
-                "1 - Remove patient by name\n" +
-                "2 - Remove highest priority patient\n" +
-                "3 - Remove all patients more urgent than a certain priority value\n" +
-                "4 - Remove all patients less urgent than a certain priority value\n" +
-                "5 - Go back\n\n" +
-                "Selection (enter a number): ");
+        System.out.println("""
+                REMOVE MENU:
+                ------------------
+                1 - Remove patient by name
+                2 - Remove highest priority patient
+                3 - Remove all patients more urgent than a certain priority value
+                4 - Remove all patients less urgent than a certain priority value
+                5 - Go back
+
+                Selection (enter a number):\s""");
 
         int removeSelectionInt = removeOptionInt.nextInt();
 
         //to add: cases when there are no patients / no existing patients with the name inputted
         switch (removeSelectionInt) {
-            case 1:
+            case 1 -> {
                 Scanner removeName = new Scanner(System.in);
-
                 System.out.println("Name: ");
                 String name = removeName.next();
+                if (queue.contains(name)) {
+                    queue.remove(name);
+                    this.removeSuccess();
+                } else {
 
-                queue.remove(name);
-                this.removeSuccess();
-                break;
-            case 2:
+                    this.removeFailure();
+
+                }
+            }
+            case 2 -> {
                 queue.removeMin();
                 this.removeSuccess();
-                break;
-            case 3:
+            }
+            case 3 -> {
                 Scanner removeMinPriority = new Scanner(System.in);
-
                 System.out.println("Threshold priority value: ");
                 double minPriority = removeMinPriority.nextDouble();
-
                 queue.removeUrgentPatients(minPriority);
                 this.removeSuccess();
-                break;
-            case 4:
+            }
+            case 4 -> {
                 Scanner removeMaxPriority = new Scanner(System.in);
-
                 System.out.println("Threshold priority value: ");
                 double maxPriority = removeMaxPriority.nextDouble();
-
                 queue.removeNonUrgentPatients(maxPriority);
                 this.removeSuccess();
-                break;
-            case 5:
-                this.mainOptions();
-                break;
-            default:
+            }
+            case 5 -> this.mainOptions();
+            default -> {
                 System.out.println("Invalid selection\n");
                 this.mainOptions();
-                break;
+            }
         }
     }
 
@@ -188,63 +174,60 @@ public class Menu {
         this.mainOptions();
     }
 
+    void removeFailure() {
+        System.out.println("Error: Patient does not exist in queue.\n");
+        this.mainOptions();
+    }
+
     //to add: cases where the patient cannot be found (in later options especially)
     void findMenu() {
 
         Scanner findOptionInt = new Scanner(System.in);
-        System.out.println("FIND MENU:\n" +
-                "------------------\n" +
-                "1 - Check if patient is in the queue\n" +
-                "2 - Find patient's priority\n" +
-                "3 - Find most urgent patient in queue\n" +
-                "4 - Go back\n\n" +
-                "Selection (enter a number): ");
+        System.out.println("""
+                FIND MENU:
+                ------------------
+                1 - Check if patient is in the queue
+                2 - Find patient's priority
+                3 - Find most urgent patient in queue
+                4 - Go back
+
+                Selection (enter a number):\s""");
 
         int findSelectionInt = findOptionInt.nextInt();
 
-        switch(findSelectionInt) {
-            case 1:
+        switch (findSelectionInt) {
+            case 1 -> {
                 Scanner findName = new Scanner(System.in);
-
                 System.out.println("Name: ");
                 String name = findName.next();
-
-                if(queue.contains(name)){
+                if (queue.contains(name)) {
                     System.out.println("Patient is in queue.");
                 } else {
                     System.out.println("Patient is not in queue.");
                 }
-
                 this.mainOptions();
-                break;
-            case 2:
-
+            }
+            case 2 -> {
                 Scanner findNamesPriority = new Scanner(System.in);
-
                 System.out.println("Name: ");
                 String namePriority = findNamesPriority.next();
-
-                if(queue.contains(namePriority)){
+                if (queue.contains(namePriority)) {
                     System.out.println("Patient's priority is " + queue.getPriority(namePriority));
                 } else {
                     System.out.println("Error: Patient does not exist");
                 }
-
                 this.mainOptions();
-                break;
-            case 3:
+            }
+            case 3 -> {
                 //add case where no patients exist in the line
                 System.out.println("Highest priority patient is " + queue.peekMin() + ", with priority value " + queue.getMinPriority());
-
                 this.mainOptions();
-                break;
-            case 4:
-                this.mainOptions();
-                break;
-            default:
+            }
+            case 4 -> this.mainOptions();
+            default -> {
                 System.out.println("Invalid selection\n");
                 this.mainOptions();
-                break;
+            }
         }
     }
 
